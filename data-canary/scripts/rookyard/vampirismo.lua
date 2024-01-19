1b1f19e11d3ec9a2171d24539b31dca3c25dc64b
konnas <gpagabriel2002@gmail.com>
2024-01-19 19:22:22 +0100

    espadachim spell vampirismo

---
+++ data-canary/scripts/rookyard/vampirismo.lua
@@ -0,0 +1,32 @@
+local condition = Condition(CONDITION_ATTRIBUTES)
+condition:setParameter(CONDITION_PARAM_TICKS, 10000)
+condition:setParameter(CONDITION_PARAM_SKILL_LIFE_LEECH_CHANCE, 100)
+condition:setParameter(CONDITION_PARAM_SKILL_LIFE_LEECH_AMOUNT, 100)
+condition:setParameter(CONDITION_PARAM_BUFF_SPELL, true)
+
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_GREEN)
+combat:setParameter(COMBAT_PARAM_AGGRESSIVE, 0)
+combat:addCondition(condition)
+
+local spell = Spell("instant")
+
+function spell.onCastSpell(creature, var)
+	return combat:execute(creature, var)
+end
+
+spell:name("Vampirismo")
+spell:words("utito tempo")
+spell:group("support", "focus")
+spell:vocation("espadachim;true")
+spell:castSound(SOUND_EFFECT_TYPE_SPELL_BLOOD_RAGE)
+spell:id(133)
+spell:cooldown(2 * 1000)
+spell:groupCooldown(2 * 1000, 2 * 1000)
+spell:level(1)
+spell:mana(1)
+spell:isSelfTarget(true)
+spell:isAggressive(false)
+spell:isPremium(true)
+spell:needLearn(false)
+spell:register()
